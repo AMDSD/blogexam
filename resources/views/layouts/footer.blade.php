a7cc061005cafff5a3886d5058240d126e81ba55
AMDSD <AMDSD>
2022-02-03 18:33:30 +0800

    blogg

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,85 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto boarder-b-2 boarder-gray-700">
+        <div>
+            <h3 Class="text-l sm:font=bold text-gray-100">
+                Pages
+            </h3>
+
+            <ul
+            class="py-4 sm:text-s pt-4 text-gray-400">
+
+                <li Class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li Class="pb-1">
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>   <li Class="pb-1">
+                    <a href="/login">
+                     login
+                    </a>
+                </li>   
+                 <li Class="pb-1">
+                    <a href="/register">
+                      register
+                    </a>
+                </li>
+            </ul>
+         
+        </div>
+
+        <div>
+            <h3 Class="text-l sm:font=bold text-gray-100">
+                Find Us
+            </h3>
+
+            <ul
+            class="py-4 sm:text-s pt-4 text-gray-400">
+
+                <li Class="pb-1">
+                    <a href="/">
+                        What we do
+                    </a>
+                </li>
+                <li Class="pb-1">
+                    <a href="/">
+                        Address
+                    </a>
+                </li>  
+                 <li Class="pb-1">
+                    <a href="/">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+         
+        </div>
+        <div>
+            <h3 Class="text-l sm:font=bold text-gray-100">
+                Latest Blog
+            </h3>
+
+            <ul
+            class="py-4 sm:text-s pt-4 text-gray-400">
+
+                <li Class="pb-1">
+                    <a href="/">
+                        Why Use Ingco Tools
+                    </a>
+                </li>
+                <li Class="pb-1">
+                    <a href="/">
+                        Why Work with us
+                    </a>
+                </li>
+               </ul>
+        </div>
+    </div>
+    <p class ="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, odio repellat laborum hic dolorum aspernatur omnis eligendi architecto aut, rerum cumque a magnam eos amet nemo tempore voluptatibus fugit sint! 2022-2023. All Rights Reserved.
+
+    </p>
+</footer>
